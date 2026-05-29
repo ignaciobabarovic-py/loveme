5c6acb0ba3846e0e6527a9222bf7c150d5a2a4eb
Ignacio <ignaciobabarovic@gmail.com>
2026-05-29 15:47:42 -0400

    fix: remove basePath for Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/proyectos/love-me',
-  assetPrefix: '/proyectos/love-me',
   images: {
     unoptimized: true,
   },
